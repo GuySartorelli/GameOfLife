b932e8343d760f71204c5013fbcfe14076f3a867
remnankell <remnankell@gs1609.AD.ECS.VUW.AC.NZ>
2018-08-20 14:42:39 +1200

    More effective pause and play

--- GameOfLifeDHH/src/GameOfLifeUI.java
+++ GameOfLifeDHH/src/GameOfLifeUI.java
@@ -24,15 +24,16 @@ import javafx.util.Duration;
 
 /**
  *  CONWAYS GAME OF LIFE
- *
+ *	@author dirglehurbleherb
  */
 
 public class GameOfLifeUI extends Application {
 	private int width = 500, height = 500;
 
-	private Group root = new Group();
-	private VBox pane = new VBox(5);
-	private Scene scene = new Scene(root, width, height);
+	private Group displayBuffer = new Group();
+	private VBox buttonsBox = new VBox(5);
+	private Scene scene = new Scene(displayBuffer, width, height);
+	private Timeline timeline = new Timeline(); 
 	private Button playButton = new Button("\u25B6");
 	private Button pauseButton = new Button("\u23F8");
 	private Button stopButton = new Button(	"\u23F9");
@@ -54,57 +55,44 @@ public class GameOfLifeUI extends Application {
 		
 		//TimelineBuilder.create().cycleCount(javafx.animation.Animation.INDEFINITE).keyFrames(frame).build().play();
 		// TimelineBuilder is deprecated, Used Timeline below instead.
-		Timeline timeline = new Timeline(); 
 		timeline.setCycleCount(Timeline.INDEFINITE); 
 		timeline.getKeyFrames().addAll(frame); 
 		timeline.setAutoReverse(true);
 //		timeline.play();
 
-		pane.getChildren().add(playButton);
-		pane.getChildren().add(pauseButton);
-		pane.getChildren().add(stopButton);
+		buttonsBox.getChildren().add(playButton);
+		buttonsBox.getChildren().add(pauseButton);
+		buttonsBox.getChildren().add(stopButton);
 
-		pane.setPadding(new Insets(10, 10, 10, 10));
-		pane.setBackground(Background.EMPTY);
-		root.getChildren().add(pane);
+		buttonsBox.setPadding(new Insets(10, 10, 10, 10));
+		buttonsBox.setBackground(Background.EMPTY);
+		displayBuffer.getChildren().add(buttonsBox);
 		
 		playButton.setStyle("-fx-font: 15 arial; -fx-base: #353535;-fx-text-fill: white; -fx-pref-width: 28px; -fx-pref-height: 28px;");
 		pauseButton.setStyle("-fx-font: 12 arial; -fx-base: #353535;-fx-text-fill: white; -fx-pref-width: 28px; -fx-pref-height: 28px;");
 		stopButton.setStyle("-fx-font: 10 arial; -fx-base: #353535;-fx-text-fill: white; -fx-pref-width: 28px; -fx-pref-height: 28px;");
 		
-		playButton(timeline);
-		pauseButton(timeline);
-		stopButton(timeline);	
+		playButton.setOnAction(this::doPlay);
+		pauseButton.setOnAction(this::doPause);
+		stopButton.setOnAction(this::doStop);	
 		
-		primaryStage.setTitle("Bug World");
+		primaryStage.setTitle("Conway's Game of Life");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}			
 	
-	public void playButton(Timeline timeline) {
-		playButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent arg0) {
-				timeline.play();
-			}
-		});
+	public void doPlay(ActionEvent act) {
+		timeline.play();
 	}
 
-	public void pauseButton(Timeline timeline) {
-		pauseButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent arg0) {
-				timeline.pause();
-			}
-		});
+	public void doPause(ActionEvent act) {
+		timeline.pause();
 	}
 	
-	public void stopButton(Timeline timeline) {
-		stopButton.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent arg0) {
-				timeline.stop();
-				System.exit(0);
-			}
-		});
+	public void doStop(ActionEvent act) {
+		timeline.stop();
+		System.exit(0);
 	}
 
 	public static void main(String[] args) {
