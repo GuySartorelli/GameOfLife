de653670a8d3e6fec0d9e672478056eb67533d4f
saadheba <saadheba@gs1807.AD.ECS.VUW.AC.NZ>
2018-08-21 16:05:20 +1200

    Commenting the cell class

--- GameOfLifeDHH/src/Cell.java
+++ GameOfLifeDHH/src/Cell.java
@@ -1,12 +1,21 @@
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
-
+/**
+ * Class to store info about the cell object and extends Rectangle
+ * @author saadheba
+ *
+ */
 public class Cell extends Rectangle {
-	
+	/**
+	 * Constaucter for the cell class
+	 * @param cellSize the width and height of the cell
+	 * @param x for x pos
+	 * @param y for y pos
+	 */
 	public Cell(int cellSize, long x, long y) {
 		super(x, y, cellSize, cellSize);
-		// if enough time add colour to cell
+	
 
 		setStroke(Color.WHITE);
 		setStrokeType(StrokeType.INSIDE);
