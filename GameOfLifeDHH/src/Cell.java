55351e57c9fc05d435dea3061180096157ac11cf
shawbeva <shawbeva@gs1803.AD.ECS.VUW.AC.NZ>
2018-08-22 15:55:53 +1200

    Colour comboBox labels

--- GameOfLifeDHH/src/Cell.java
+++ GameOfLifeDHH/src/Cell.java
@@ -5,7 +5,10 @@ import java.util.Map;
 
 import javafx.scene.control.ColorPicker;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Paint;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 /**
@@ -15,9 +18,12 @@ import javafx.scene.shape.StrokeType;
  */
 public class Cell extends Rectangle {
 
-	private static ColorOption defaultOption = new ColorOption("Lifespan",Color.YELLOW, Color.GREEN, Color.BLUE, Color.RED);
+	//	private static ColorOption defaultOption = new ColorOption("Lifespan",Color.YELLOW, Color.GREEN, Color.BLUE, Color.RED);
 	private int lifespan = 0;
 	private Game game;
+	private static Color custom = Color.CHOCOLATE;
+
+
 
 	/**
 	 * Constructor for the cell class
@@ -100,7 +106,7 @@ public class Cell extends Rectangle {
 		double red = Math.random();
 		double green = Math.random();
 		double blue = Math.random();
-		
+
 		if((red != 0.0 && green !=0.0 && blue != 0.0)||((red != 1.0 && green !=1.0 && blue != 1.0))){
 			this.setFill(Color.color(red, green, blue));
 		}
@@ -111,26 +117,37 @@ public class Cell extends Rectangle {
 		//colourRuleNumNeighbours();
 		//colourRuleRandom();
 	}
-	
+
 	/**
 	 * Returns a list of all color options for use in a dropbox
 	 * @return List<ColorOption> list of all rules
 	 */
 	public static Map<String,Paint[]> getColorRules() {
 		Map<String,Paint[]> colorRules = new HashMap<String,Paint[]>();
-		colorRules.put("Lifespan",new Paint[] {Color.YELLOW, Color.GREEN, Color.BLUE, Color.RED});
-		colorRules.put("NumNeighbours",new Paint[] {Color.YELLOW, Color.ORANGE, Color.RED});
-		//colorRules.put("Random",Color.RED, Color.BLUE));
+		colorRules.put("Lifespan",new Paint[] {Color.YELLOW, 
+				new LinearGradient(0,0,1,1,true, CycleMethod.REPEAT,
+						new Stop(0,Color.CYAN), 
+						new Stop(0.5,Color.BLUE),
+						new Stop(1,Color.MAGENTA)),
+						Color.RED});//Color.YELLOW, Color.GREEN, Color.BLUE, Color.RED
+		colorRules.put("Neighbours",new Paint[] {Color.YELLOW, Color.ORANGE, Color.RED});
+		colorRules.put("Random", new Paint [] {new LinearGradient(0,0,1,1,true, CycleMethod.REPEAT,
+						new Stop(0,Color.GOLD), 
+						new Stop(1,Color.AQUAMARINE))});
+		colorRules.put("Custom",new Paint[] {custom});
 		//colorRules.add(defaultOption);
 		return colorRules;
 	}
-	
+
 	/**
 	 * Returns a list of all color options for use in a dropbox
 	 * @return List<ColorOption> list of all rules
 	 */
-	public static ColorOption getDefaultColorRule() {
-		return defaultOption;
-		
+	//	public static ColorOption getDefaultColorRule() {
+	//		return defaultOption;
+
+	//	}
+	public static void setCustom(Color custom) {
+		Cell.custom = custom;
 	}
 }
